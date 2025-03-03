b0e6523c7d88051e7be5dac631886d7f64d6ea3d
Muhammad Ahmad <mahmad92001@gmail.com>
2025-03-03 15:48:31 +0500

    80% done

--- script.js
+++ script.js
@@ -4,3 +4,24 @@ function toggleMenu() {
   menu.classList.toggle("open");
   icon.classList.toggle("open");
 }
+var typed = new Typed("#typed-text", {
+  strings: ["Android Developer", "AI Engineer", "Freelancer"], 
+  typeSpeed: 50, 
+  backSpeed: 25, 
+  loop: true 
+});
+const typedText = document.getElementById("typing-text");
+  const text = typedText.innerHTML; // Get text from HTML
+  typedText.innerHTML = ""; // Clear the text before typing starts
+  let charIndex = 0;
+  const typingSpeed = 100;
+
+  function typeText() {
+    if (charIndex < text.length) {
+      typedText.innerHTML += text.charAt(charIndex);
+      charIndex++;
+      setTimeout(typeText, typingSpeed);
+    }
+  }
+
+  typeText();
